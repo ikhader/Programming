5ec282fdffa78d3913ee8f98e60284b0496b6765
ikhader <ikhader@me.com>
2017-03-28 09:12:46 -0500

    Update closures.py

--- python/closures.py
+++ python/closures.py
@@ -1,3 +1,9 @@
+
+
+#
+#In a nested funcs using local varaibles of topmost func in inner funcs
+#
+
 print "Hello World!\n"
 class Employee:
   def __init__ (self, first, last):
@@ -17,3 +23,20 @@ print e1.fullname()
 #email was supposed to be a vairable but changed to method later..
 #earlier usage of people used "email" as a varaible. Method to prefixed "property"will act as a varaible
 print e1.email
+
+
+def print_msg(m):
+  def log():
+    print ("Accessing variable 'print_msg' Hello world: {}", m)
+  return log()
+
+print_msg("test message - 1")
+
+def print_msg_1(m):
+  hello = "go to hell!!!"
+  def log():
+    print ("variable 'print_msg_1': {} {}", m, hello)
+  return log # this is changed from the upper on
+
+t = print_msg_1("test message - 2")
+t()
