36cee9fc81f4ad50d6825cbec2c5a93fb8141dd3
ikhader <ikhader@me.com>
2017-03-28 09:14:46 -0500

    Create gen_1.py

---
+++ python/gen_1.py
@@ -0,0 +1,48 @@
+
+#generators are nothing but 'yeild'
+# this will help in memory, stack will not grow if squares to be caluclated 100000 numbers
+
+def square(n):
+  result = []
+  for i in n:
+    result.append(i*i)
+  return result
+
+my_nums = square([1,2,3, 4])
+print "Non-genrator format: "
+print my_nums
+
+
+
+def gen_square(n):
+  for i in n:
+    yield(i*i)
+
+print "genrator format: "
+num = gen_square([1,2,3, 4])
+print next(num)
+print next(num)
+print next(num)
+print next(num)
+
+print("If we print one more time of generator we will 'StopIteration' error")
+
+nums = gen_square([10, 20, 30, 40])
+print ("Generator usecase -1")
+for n in nums:
+  print n
+
+import sys
+print ('Memory (Before): {0}Mb'.format(sys.getsizeof([])))
+
+print ("Generator usecase -2")
+my_nums = (x*x for x in [1,2,3])
+print my_nums
+for n in my_nums:
+  print n
+
+print ("Generator usecase -3")
+my_nums = (x*x for x in [1,2,3])
+print list(my_nums)
+
+
