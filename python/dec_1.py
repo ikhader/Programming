a66361a4b65b1f7897daa9b430e1953b51dfdbee
ikhader <ikhader@me.com>
2017-03-29 13:36:26 -0500

    Update dec_1.py

--- python/dec_1.py
+++ python/dec_1.py
@@ -1,5 +1,15 @@
 
+
 #decorators takes a function (as a funciton argument) and add soem "decoration" then return it
+# decorating functions/outermost function, accecpts function pointer(first class function) as an argument
+# This first class function will be used in inner functions and invoked from this inner function
+#But in closures, both innner & outer functinos will have same argument list(strings as example)
+# Then this inner function will use that outermost func arguments(strings as example)
+
+# Common things in closure & decorators:
+#  @ both have nested functions
+#  @ both retunrs inner most functions
+
 
 def make_pretty(func):
   def inner():
@@ -12,3 +22,4 @@ def ordinary():
 
 f = make_pretty(ordinary)
 f()
+
