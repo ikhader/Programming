2faffbd1b952ae975bd745053c7fc41196866bce
ikhader <ikhader@me.com>
2018-05-29 14:33:13 -0700

    Update class_ex1.cpp
    added destrucots notes as well

--- cpp/class_ex1.cpp
+++ cpp/class_ex1.cpp
@@ -7,6 +7,7 @@ public:
     virtual void show() { cout<<" In Base \n"; }
     void overload_show() { cout<<" In Base overload show \n"; }
     Base(){ cout<<"Base constructor \n"; }
+    virtual ~Base(){cout<<"Base Destructor \n";}
 };
 
 class Derived: public Base
@@ -15,6 +16,7 @@ public:
     void show() { cout<<"In Derived \n"; }
     void overload_show() { cout<<" In DERV overload show \n"; }
     Derived(){ cout<<"Derived constructor \n"; }
+    ~Derived(){ cout<<"Derived Destructor \n"; }
 };
 
 
@@ -24,38 +26,75 @@ int main()
   Base *p1 = new Base();
   p1->show();
   p1->overload_show();
+  delete p1;
+  
   
   cout <<"------------------------------------ \n";
   cout<<"creating base *p = new derv:" <<endl;
   Base *p2 = new Derived();
   p2->show();
   p2->overload_show();
+  delete p2;
   
   cout <<"------------------------------------ \n";
   cout<<"creating derv *p = new derv:" <<endl;
   Derived *p3 = new Derived();
   p3->show();
   p3->overload_show();
+  delete p3;
+  
+  //Derived *p4 = new Base(); --> will not compile: Invalid conversion
   
 return 0;
 }
 
 /*
-Expected output:
+expected output
+With NO "virtual" keyword in base class:
+################################################
+
 creating base *p = new base:
 Base constructor 
  In Base 
  In Base overload show 
+Base Destructor 
 ------------------------------------ 
 creating base *p = new derv:
 Base constructor 
 Derived constructor 
 In Derived 
  In Base overload show 
+Base Destructor 
 ------------------------------------ 
 creating derv *p = new derv:
 Base constructor 
 Derived constructor 
 In Derived 
  In DERV overload show 
+Derived Destructor 
+Base Destructor 
+###########################################################
+
+With VIRTUAL destructor:
+creating base *p = new base:
+Base constructor 
+ In Base 
+ In Base overload show 
+Base Destructor 
+------------------------------------ 
+creating base *p = new derv:
+Base constructor 
+Derived constructor 
+In Derived 
+ In Base overload show 
+Derived Destructor 
+Base Destructor 
+------------------------------------ 
+creating derv *p = new derv:
+Base constructor 
+Derived constructor 
+In Derived 
+ In DERV overload show 
+Derived Destructor 
+Base Destructor 
 */
